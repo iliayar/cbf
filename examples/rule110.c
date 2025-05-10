591d988652f629e823fe46bb5c6b5385ee7ca58d
iliayar <iliayar3@gmail.com>
2025-05-11 01:17:03 +0300

    fixup! feat(examples): Add Rule 110 example

--- examples/rule110.c
+++ examples/rule110.c
@@ -1,49 +1,40 @@
+void print_array(int a[200]) {
+    int SIZE = 200;
+
+    int i = 0;
+    while (SIZE - i) {
+        if (a[i]) {
+            write('#');
+        } else {
+            write('_');
+        }
+        i = i + 1;
+    }
+    write('\n');
+}
 
 int main() {
-    int cur[100];
-    int next[100];
+    int SIZE = 200;
+    int cur[200];
+    int next[200];
 
-    // cur[50 - 16 + 1] = 0;
-    // cur[50 - 16 + 2] = 0;
-    // cur[50 - 16 + 3] = 0;
-    // cur[50 - 16 + 4] = 1;
-    // cur[50 - 16 + 5] = 0;
-    // cur[50 - 16 + 6] = 0;
-    // cur[50 - 16 + 7] = 1;
-    // cur[50 - 16 + 8] = 1;
-    // cur[50 - 16 + 9] = 0;
-    // cur[50 - 16 + 10] = 1;
-    // cur[50 - 16 + 11] = 1;
-    // cur[50 - 16 + 12] = 1;
-    // cur[50 - 16 + 13] = 1;
-    // cur[50 - 16 + 14] = 1;
-
-    cur[100 - 12 + 1] = 0;
-    cur[100 - 12 + 2] = 0;
-    cur[100 - 12 + 2] = 0;
-    cur[100 - 12 + 3] = 1;
-    cur[100 - 12 + 4] = 1;
-    cur[100 - 12 + 5] = 1;
-    cur[100 - 12 + 6] = 0;
-    cur[100 - 12 + 7] = 1;
-    cur[100 - 12 + 8] = 1;
-    cur[100 - 12 + 9] = 1;
+    cur[SIZE - 12 + 1] = 0;
+    cur[SIZE - 12 + 2] = 0;
+    cur[SIZE - 12 + 2] = 0;
+    cur[SIZE - 12 + 3] = 1;
+    cur[SIZE - 12 + 4] = 1;
+    cur[SIZE - 12 + 5] = 1;
+    cur[SIZE - 12 + 6] = 0;
+    cur[SIZE - 12 + 7] = 1;
+    cur[SIZE - 12 + 8] = 1;
+    cur[SIZE - 12 + 9] = 1;
     int iterations = 0;
 
-    while (100 - iterations) {
-        int i = 0;
-        while (100 - i) {
-            if (cur[i]) {
-                write('#');
-            } else {
-                write('_');
-            }
-            i = i + 1;
-        }
-        write('\n');
+    while (SIZE - iterations) {
+        print_array(cur);
 
-        i = 1;
-        while (100 - 1 - i) {
+        int i = 1;
+        while (SIZE - 1 - i) {
             if (cur[i - 1]) {
                 if (cur[i]) {
                     if (cur[i + 1]) {
